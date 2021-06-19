5a8b4e0d09f6473793fea71dc2619caee5eb8b03
FrankPunyaBarang <181112221@students.mikroskil.ac.id>
2021-06-19 21:21:05 +0700

    Tugas selesai

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <HelloWorld  />
+  <router-link to="/">Halaman Login</router-link>&nbsp;
+  <router-link to="/todo">Halaman Todo</router-link>&nbsp;
+  <router-link to="/user">Halaman User</router-link>&nbsp;
+  <router-view to="/user"></router-view>
+
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todo from './components/Todo.vue'
+import User from './components/User.vue'
 </script>
-
